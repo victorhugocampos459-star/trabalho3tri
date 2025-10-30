ad6158db276c6b493f0a57acca70a24a332f0934
victorhugocampos459-star <victorhugocampos459@gmail.com>
2025-10-30 09:33:36 -0300

    Fix prime number check logic in ex06_primo.c

--- ex06_primo.c
+++ ex06_primo.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
+#include <math.h>
 
 int main() {
-    int n, i, ehPrimo = 1;
+    int n, i, primo = 1;
 
     printf("Digite um numero maior ou igual a 2: ");
     scanf("%d", &n);
 
-    for (i = 2; i < n; i++) {
+    for (i = 2; i <= sqrt(n); i++) {
         if (n % i == 0) {
-            ehPrimo = 0;
+            primo = 0;
             break;
         }
     }
 
-    if (ehPrimo == 1)
+    if (primo == 1)
         printf("%d e primo\n", n);
     else
         printf("%d nao e primo, menor divisor: %d\n", n, i);
